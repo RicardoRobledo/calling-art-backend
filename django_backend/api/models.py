5deb74e3bdc7375a4a0c828f8e935ddc3216c50f
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-06 21:35:42 -0500

    feat(api): 'models.py' module was modified
    'Image' model was added with its migration

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -12,3 +12,15 @@ class User(AbstractBaseUser):
     description = models.CharField(max_length=200, null=False, blank=False,)
     icon = models.CharField(max_length=200, null=False, blank=False,)
     created_at = models.DateTimeField(auto_now_add=True)
+
+
+# -------------------------------------------------------------
+#                             Image
+# -------------------------------------------------------------
+class Image(models.Model):
+    
+    title = models.CharField(max_length=20, null=False, blank=False)
+    description = models.CharField(max_length=200, null=False, blank=False)
+    link = models.CharField(max_length=200, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    id_user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
