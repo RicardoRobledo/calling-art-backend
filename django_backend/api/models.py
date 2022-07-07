554fdba922121b3b33431b5254e4268973ea8b93
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-06 22:53:49 -0500

    feat(api): 'models.py' module was modified
    'Category' model was added with its migration

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -2,10 +2,24 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
 
+__author__ = 'Ricardo'
+__version__ = '0.1'
+
+
 # -------------------------------------------------------------
 #                             User
 # -------------------------------------------------------------
 class User(AbstractBaseUser):
+    """
+    This model define an user
+
+    Attributes:
+        email (str): email of the user
+        username (str): username of the user
+        description (str): description of the user
+        icon (str): url of the icon
+        created_at (datetime): creation date
+    """
     
     email = models.EmailField(unique=True, null=False, blank=False,)
     username = models.CharField(max_length=20, null=False, blank=False,)
@@ -18,9 +32,35 @@ class User(AbstractBaseUser):
 #                             Image
 # -------------------------------------------------------------
 class Image(models.Model):
+    """
+    This model define an Image
+
+    Args:
+        title (str): title of the image
+        description (str): description of the image
+        link (str): url of the image
+        created_at (datetime): creation date
+        id_user (int): id to do refernce of the user
+    """
     
     title = models.CharField(max_length=20, null=False, blank=False)
     description = models.CharField(max_length=200, null=False, blank=False)
     link = models.CharField(max_length=200, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     id_user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+
+
+# -------------------------------------------------------------
+#                           Category
+# -------------------------------------------------------------
+class Category(models.Model):
+    """
+    This model define a category
+
+    Args:
+        category (datetime): creation date
+        created_at (str): description of the image
+    """
+    
+    category = models.CharField(max_length=15, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
