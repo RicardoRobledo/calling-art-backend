55c025160debe892ebb46b08c214cd9773059775
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-06 21:31:07 -0500

    feat(api): 'models.py' module was modified
    'User' model was added with its migration

--- django_backend/api/models.py
+++ django_backend/api/models.py
@@ -1,2 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+
+
+# -------------------------------------------------------------
+#                             User
+# -------------------------------------------------------------
+class User(AbstractBaseUser):
+    
+    email = models.EmailField(unique=True, null=False, blank=False,)
+    username = models.CharField(max_length=20, null=False, blank=False,)
+    description = models.CharField(max_length=200, null=False, blank=False,)
+    icon = models.CharField(max_length=200, null=False, blank=False,)
+    created_at = models.DateTimeField(auto_now_add=True)
