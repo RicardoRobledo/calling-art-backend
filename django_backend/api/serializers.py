2f4e7c3d45f4234602c6b135e5b03e0d72d43ded
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-08 17:29:50 -0500

    feat(api): 'serializers.py' module was added

---
+++ django_backend/api/serializers.py
@@ -0,0 +1,16 @@
+"""
+This module define our serializers
+"""
+
+
+from rest_framework import serializers
+from .models import(
+    User,
+    Category,
+    Image,
+    ImageCategory,
+)
+
+
+__author__ = "Ricardo Robledo"
+__version__ = "0.1"
