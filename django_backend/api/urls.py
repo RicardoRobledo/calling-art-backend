f229033fe7f62bcf797fc583c4016ae4949e83dc
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-08 18:26:46 -0500

    feat(api): 'urls.py' module was created

---
+++ django_backend/api/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import(
+    UserViewSet,
+    CategoryViewSet,
+    ImageViewSet,
+    ImageCategoryViewSet,
+)
+
+
+__author__ = 'Ricardo'
+__version__ = '0.1'
+
+
+urlpatterns = []
