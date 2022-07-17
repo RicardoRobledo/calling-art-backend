53f3016cde47a6f4292fdc8fe6f66ae1cd3a94c1
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-16 19:37:56 -0500

    feat(apps): 'User' model was moved
    'User' model was put inside 'models.py' module in 'users' app

--- django_backend/apps/users/models.py
+++ django_backend/apps/users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+
+__author__ = 'Ricardo'
+__version__ = '0.1'
+
+
+# -------------------------------------------------------------
+#                             User
+# -------------------------------------------------------------
+
+
+class User(AbstractBaseUser):
+    """
+    This model define an user
+
+    Attributes:
+        email (str): email of the user
+        username (str): username of the user
+        description (str): description of the user
+        icon (str): url of the icon
+        created_at (datetime): creation date
+    """
+    
+    USERNAME_FIELD = 'username'
+
+    email = models.EmailField(unique=True, null=False, blank=False,)
+    username = models.CharField(max_length=20, null=False, blank=False,)
+    description = models.CharField(max_length=200, null=False, blank=False,)
+    icon = models.CharField(max_length=200, null=False, blank=False,)
+    created_at = models.DateTimeField(auto_now_add=True)
