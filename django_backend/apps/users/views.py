349a9a62bdf19bf4ad6b10599959f1a386be2908
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-20 21:08:30 -0500

    feat(apps): 'accounts' app was added

--- django_backend/apps/users/views.py
+++ django_backend/apps/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from apps.users.models import User
 from apps.users.user_serializers import UserSerializer
@@ -17,3 +18,15 @@ class UserViewSet(viewsets.ModelViewSet):
     
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+# -------------------------------------------------------------
+#                       Login view set
+# -------------------------------------------------------------
+
+
+class LoginViewSet(ObtainAuthToken):
+    
+    def get(self, request, *args, **kwargs):
+        
+        print(request.data)
