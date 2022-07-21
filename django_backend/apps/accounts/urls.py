1064698a4a67e4d5780e8590efe9223560c6ffed
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-20 22:51:05 -0500

    feat(django_backend): 'base' apps was made up

--- django_backend/apps/accounts/urls.py
+++ django_backend/apps/accounts/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from .views import LoginView
 
 
+__author__ = 'Ricardo'
+__version__ = '0.1'
+
+
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login')
 ]
