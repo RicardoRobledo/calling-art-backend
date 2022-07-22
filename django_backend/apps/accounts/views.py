d44da3b9a554fc754f78ce37f030424ae52363e2
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-22 17:11:09 -0500

    feat(users): 'models.py' module was modified
    'UserManager' class has been added

--- django_backend/apps/accounts/views.py
+++ django_backend/apps/accounts/views.py
@@ -1,7 +1,9 @@
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 
 from apps.base.utils import format_response
+from apps.users.models import User
 
 
 # ---------------------------------------------
@@ -21,9 +23,12 @@ class LoginView(ObtainAuthToken):
         """
         
         login_serializer = self.serializer_class(data=request.data)
-        
-        if login_serializer.is_valid():
 
+        if login_serializer.is_valid():
+            user = login_serializer.validated_data['user']
+            print(user)
+            print(User.objects.all())
+            
             return format_response(
                 {'message':'Se ha iniciado sesion'},
                 status.HTTP_200_OK
--- django_backend/apps/users/models.py
+++ django_backend/apps/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
 
 __author__ = 'Ricardo'
@@ -11,7 +12,27 @@ __version__ = '0.1'
 # -------------------------------------------------------------
 
 
-class User(AbstractBaseUser):
+class UserManager(BaseUserManager):
+
+
+    def create_user(self, username, password, email, is_staff, is_active, is_superuser=False):
+        user = self.model(
+            username=username,
+            email=email,
+            is_staff=is_staff,
+            is_active=is_active,
+            is_superuser=is_superuser,
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+    def create_superuser(self, username, password, email):
+        return self.create_user(username, password, email, True, True, True)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     """
     This model define an user
 
@@ -23,10 +44,18 @@ class User(AbstractBaseUser):
         created_at (datetime): creation date
     """
     
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+    
     USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
 
+    objects = UserManager()
     email = models.EmailField(unique=True, null=False, blank=False,)
     username = models.CharField(unique=True, max_length=20, null=False, blank=False,)
     description = models.CharField(max_length=200, null=False, blank=False,)
     icon = models.CharField(max_length=200, null=False, blank=False,)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
