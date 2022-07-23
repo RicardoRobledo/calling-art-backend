7ea02fbe8ffb1b095b9ee5be5e926c0a69251795
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-07-22 21:00:44 -0500

    feat(users): 'admin.py' module was updated
    'UserAdmin' class was added to manage the edit

--- django_backend/apps/users/admin.py
+++ django_backend/apps/users/admin.py
@@ -1,2 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
 from .models import User
+
+
+class UserAdmin(BaseUserAdmin):
+    
+    list_display = ('id', 'username',)
+    
+    fieldsets = (
+    (
+        None, {'fields': ('username', 'password')}),
+    )
+
+
+admin.site.register(User, UserAdmin)
