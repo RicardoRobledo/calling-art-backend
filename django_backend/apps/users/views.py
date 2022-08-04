6298a4f9f8910cc69602be334480654fbfd73869
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-08-04 14:57:59 -0500

    feat(users): 'views.py' module was modified
    'permission_classes' attribute was modified to has 'IsAuthenticatedOrReadOnly' permission

--- django_backend/apps/users/views.py
+++ django_backend/apps/users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from apps.users.models import User
 from apps.users.user_serializers import UserSerializer
@@ -15,5 +16,6 @@ __version__ = "0.1"
 
 class UserViewSet(viewsets.ModelViewSet):
     
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     serializer_class = UserSerializer
     queryset = User.objects.all()
