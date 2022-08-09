6e5c79fc6f4f7000d628795ec4919e48cec16bee
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-08-08 20:42:48 -0500

    feat(users): 'apps.py' module was modified
    'django_backend.apps' path has been added

--- django_backend/apps/users/apps.py
+++ django_backend/apps/users/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.users'
+    name = 'django_bapps.users'
+    name = 'django_backend.apps.users'
