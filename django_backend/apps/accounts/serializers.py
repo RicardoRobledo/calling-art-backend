d43273cab0a10f7187bb60e896565dbbf6b0ed83
RicardoRobledo <RicardoProgrammer44@gmail.com>
2022-08-14 17:43:10 -0500

    feat(accounts): 'serializers.py' module has been modified
    'CustomTokenObtainPairSerializer' class was added

--- django_backend/apps/accounts/serializers.py
+++ django_backend/apps/accounts/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from apps.users.models import User
 
@@ -12,3 +13,18 @@ class UserTokenSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'email', 'description', 'icon')
+
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        token['email'] = user.email
+        token['icon'] = user.icon
+        token['description'] = user.description
+
+        return token
