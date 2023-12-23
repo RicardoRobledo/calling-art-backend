ec05ad4e521def4714d714c575fa3884b5412091
Ricardo_Sao <99063411+RicardoRobledo@users.noreply.github.com>
2023-12-22 21:35:41 -0600

    Update base.py

--- django_backend/django_backend/settings/base.py
+++ django_backend/django_backend/settings/base.py
@@ -17,7 +17,8 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-# Use this if you have more folders in your setting files
+# Use one of this if you have more folders in your setting files
+# BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
